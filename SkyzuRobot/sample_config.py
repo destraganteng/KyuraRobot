ce40204c271f68bea9c0a01d29df4d141758c1f4
Kyuraxp <96605016+Kyuraxp@users.noreply.github.com>
2021-12-27 13:42:08 +0700

    Update sample_config.py

--- SkyzuRobot/sample_config.py
+++ SkyzuRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/SkyzuRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/KyuraRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -19,7 +19,7 @@ class Config(object):
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "Skyzusupport"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "Kyurasupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001432609692
     )  # Prints any new group the bot is added to, prints just the name and ID.
