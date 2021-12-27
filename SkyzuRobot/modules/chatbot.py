f48a8830c0730734cf6dffa53bb7217aead8e3ec
Kyuraxp <96605016+Kyuraxp@users.noreply.github.com>
2021-12-27 14:35:04 +0700

    Update chatbot.py

--- SkyzuRobot/modules/chatbot.py
+++ SkyzuRobot/modules/chatbot.py
@@ -54,7 +54,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Skyzu Chatbot disable by {}.".format(
+                "Kyura Chatbot disable by {}.".format(
                     mention_html(user.id, user.first_name)
                 ),
                 parse_mode=ParseMode.HTML,
@@ -82,7 +82,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Skyzu Chatbot enable by {}.".format(
+                "Kyura Chatbot enable by {}.".format(
                     mention_html(user.id, user.first_name)
                 ),
                 parse_mode=ParseMode.HTML,
