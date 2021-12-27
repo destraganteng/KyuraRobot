ea14de12224681aafd89d536a165394899e5cae6
Kyuraxp <96605016+Kyuraxp@users.noreply.github.com>
2021-12-28 02:00:43 +0700

    Update global_bans.py

--- SkyzuRobot/modules/global_bans.py
+++ SkyzuRobot/modules/global_bans.py
@@ -547,7 +547,7 @@ you and your groups by removing spam flooders as quickly as possible.
 
 This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
 *What is SpamWatch?*
-SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
+SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/d841005eca08d0ed2ef36.jpg)
 Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
 *Note:* Users can appeal spamwatch bans at @SpamwatchSupport
 """
